1eba31c64eb2bb81a899deea42ca9247285c8599
Adrián Serrano <adrianno_3@hotmail.com>
2022-03-23 17:46:32 +0100

    server & user-model changes

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
   {
+    _id: string,
     firstName: {
       type: String,
-      required: [true, "The first name is required"],
       trim: true,
     },
     lastName: {
       type: String,
-      required: [true, "The last name is required"],
       trim: true,
     },
     email: {
@@ -24,11 +22,7 @@ const UserSchema = new mongoose.Schema(
         message: (props) => `The email ${props.value} is not valid`,
       },
     },
-    password: {
-      type: String,
-      required: [true, "The password is required"],
-      minlength: [8, "The password is too short"],
-    },
+
     speaks: [
       {
         type: String,
@@ -45,24 +39,6 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-
-UserSchema.pre("save", async function passwordPreSave(next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-
-  try {
-    const hash = await bcrypt.hash(this.password, 12);
-    this.password = hash;
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
-UserSchema.methods.comparePassword = function comparePassword(candidate) {
-  return bcrypt.compare(candidate, this.password);
-};
 
 const UserModel = new mongoose.model("user", UserSchema);
 
--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const { json } = require("body-parser");
+const cors = require("cors");
 
+const userRouter = require("./routes/user-routes");
 const bookRouter = require("./routes/book-routes");
+const config = require("./config/config");
 
 const app = express();
 
@@ -11,6 +14,12 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(json());
 
-app.use(bookRouter);
+app.use(
+  cors({
+    origin: config.client.URL,
+  }),
+);
+
+app.use(userRouter);
 
 module.exports = app;
